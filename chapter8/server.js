32bb5f0fc23da4b617ca7dd31e18f2d461fe5713
Olmsted <912143546@qq.com>
2017-02-10 01:57:24 +0800

    chapter8

---
+++ chapter8/server.js
@@ -0,0 +1,44 @@
+var http=require('http')
+var fs=require('fs')
+var server=http.createServer();
+
+server.on('request',function(req,res) {
+	var file=fs.createWriteStream('./request.log');
+	console.log('request');
+	console.log(req.url);
+	var head=JSON.stringify(req.headers)
+	if(req.url!='/favicon.ico'){
+		file.write('url:'+req.url+'\n')
+		res.write('url:'+req.url+'\n')
+		console.log(head);
+		console.log('write in');
+		res.write('header:'+head+'\n')
+		file.write('header:'+head+'\n')
+		
+	}
+	req.on('data',function (data) {
+		console.log('server get:',decodeURIComponent(data))
+	})
+	req.on('end',function () {
+		console.log('data sent all');
+	})
+	res.end()
+	// console.log();
+})
+
+server.listen(1337,'127.0.0.1',function () {
+	console.log('running port 1337');
+
+	// setTimeout(function () {
+		// server.close()
+	// }, 30*1000);
+})
+
+// server.setTimeout(10*1000,function (socket) {console.log(socket)});
+
+server.on('close',function () {
+	console.log('server closed');
+})
+server.on('error',function (err) {
+	console.log('err',err.code);
+})
